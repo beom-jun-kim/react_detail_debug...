ade90620b2ae26cc606869ed8a44f86021e7305e
kimbeomjun <gimb52043@gmail.com>
2022-08-08 14:39:00 +0900

    react_detail_debuging

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -3,7 +3,7 @@ import { useParams } from "react-router-dom";
 
 function Detail() {
   const { id } = useParams();
-  const [movies, setMovies] = useState([]);
+  const [movie, setMovies] = useState([]);
   const getMovie = async () => {
     const json = await (
       await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
@@ -16,14 +16,9 @@ function Detail() {
 
   return (
     <div>
-      {movies &&
-        movies.map((movie) => (
-          <div key={movie.id}>
-            <img src={movie.medium_cover_image} alt={movie.title} />
-            <h2>{movie.title}</h2>
-            <p>{movie.summary}</p>
-          </div>
-        ))}
+      <img src={movie.medium_cover_image} alt={movie.title} />
+      <h2>{movie.title}</h2>
+      <p>{movie.description_intro}</p>
     </div>
   );
 }
